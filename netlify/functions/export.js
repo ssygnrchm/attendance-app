bfcfa92200ed7fed4aef61696cf137d4dcdfec6f
Sisy Agnia <45039754+ssygnrchm@users.noreply.github.com>
2025-07-17 09:53:35 +0700

    Update export.js

--- netlify/functions/export.js
+++ netlify/functions/export.js
@@ -3,7 +3,21 @@ const { google } = require("googleapis");
 // const path = require("path");
 const fs = require("fs");
 
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+const serviceAccount = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+};
+
+
+const credentials = serviceAccount;
 
 // const base64 = process.env.GOOGLE_SERVICE_ACCOUNT_BASE64;
 // const jsonString = Buffer.from(base64, 'base64').toString('utf-8');
